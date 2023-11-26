b73b186430d3f02789783045d50528e83784f52a
Lucas Martins <lucastins555@gmail.com>
2023-11-26 14:29:19 -0300

    Finalização do banner e linkagem da home page com fab four page

---
+++ assets/js/index.js
@@ -0,0 +1,26 @@
+//fnção de resposta para salvar a resposta da pessoa que aceita os cookies de site
+if (!localStorage.getItem("cookiesAccepted")) { 
+  var cookieMessage = document.getElementById('cookie-notification');  
+  var closeCookie = document.getElementById('cookie-notification-close');
+  
+  cookieMessage.style.display = 'block';  
+  closeCookie.addEventListener("click", function(e) {  
+    e.preventDefault();
+    localStorage.setItem("cookiesAccepted", true);
+    
+    cookieMessage.style.display = 'none';
+  });
+}
+
+function bannerSwitcher() {
+  next = $('.sec-1-input').filter(':checked').next('.sec-1-input');
+  if (next.length) next.prop('checked', true);
+  else $('.sec-1-input').first().prop('checked', true);
+}
+
+var bannerTimer = setInterval(bannerSwitcher, 5000);
+
+$('nav .controls label').click(function() {
+  clearInterval(bannerTimer);
+  bannerTimer = setInterval(bannerSwitcher, 5000)
+});
